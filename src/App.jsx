2d9a570ce7e93acc10d92d606eb07cc3f49b209e
Ben Johnston <admiralackbar411@gmail.com>
2024-06-18 15:39:08 +0100

    Initial commit with fonts

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { evaluate } from "mathjs";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="calculator">
+        <h3>CASIO</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
