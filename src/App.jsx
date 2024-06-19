4d00a133fd1e0cc9df1c02a795a7008bc7c06c00
Ben Johnston <admiralackbar411@gmail.com>
2024-06-19 14:38:19 +0100

    Slight UI changes and added code for the last result and Ans button

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 
 function App() {
   const [sum, setSum] = useState("");
+  const [lastResult, setLastResult] = useState(null);
   const simpleButtons = [
     "7",
     "8",
@@ -27,16 +28,44 @@ function App() {
     "=",
   ];
 
+  // Used to convert symbols to actual text that can be used in evaluate.
+  const operatorMapping = {
+    "÷": "/",
+    X: "*",
+    x10x: "e",
+  };
+
   const handleClick = (string) => {
-    if (string === "AC") {
-      setSum([]);
-      return;
+    // Each switch statement does a specific function related to that button,
+    // else it uses the default which just adds the value of string to sum
+    switch (string) {
+      case "AC":
+        setSum([]);
+        break;
+      case "DEL":
+        setSum(sum.slice(0, -1));
+        break;
+      case "=":
+        const sumString = sum
+          .map((item) => operatorMapping[item] || item)
+          .join("");
+        const result = evaluate(sumString);
+        console.log(result);
+        setSum([result]);
+        setLastResult(result);
+        break;
+      case "Ans":
+        if (lastResult !== null) {
+          let storedEval = [...sum, lastResult];
+          setSum(storedEval);
+          console.log(sum);
+        }
+        break;
+      default:
+        let storedEval = [...sum, string];
+        setSum(storedEval);
+        break;
     }
-    let storedEval = [...sum, string];
-    setSum(storedEval);
-    // if user enters =
-    // pass the sum into the evalute function
-    // setSum the response so it replaces the screen with the result
   };
 
   return (
